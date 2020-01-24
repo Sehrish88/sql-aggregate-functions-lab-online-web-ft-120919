0fec0960b144f961da614c1fa29fe4d3eed6dc9b
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-24 21:24:04 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,13 +9,14 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  puts "SELECT AVG(gpa) FROM students;"
+  return puts "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students;"
+  return "SELECT SUM(tardies) FROM students;"
   
 end
 
 def average_gpa_for_9th_grade
+  
 end
